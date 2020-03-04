edce6cc6a37d6e83d9353df93051303ef92e787b
Maz47 <maz47.dev@gmail.com>
2020-03-04 15:12:20 +0100

    init

---
+++ app/src/main/java/org/maz/com/kotlin_app_bar_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.maz.com.kotlin_app_bar_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
